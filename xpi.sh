bfa968ab1699b611dcd79cc96f45a0d60d8bdfbf
Andrey Voronov <vurmux@gmail.com>
2017-06-06 13:05:58 +0300

    Initial commit

---
+++ xpi.sh
@@ -0,0 +1,79 @@
+# External repositories
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
+# Update/upgrade
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
+
+# Package installation
+sudo apt-get install -y --force-yes \
+	tmux \
+	cmake \
+	gcc \
+	g++ \
+	gitk \
+	emacs24 \
+	vim \
+	mtpaint \
+	sqlite3 \
+	openssh-client \
+	openssh-server \
+	pep8 \
+	pylint \
+	python3 \
+	python3-crypto \
+	python3-requests \
+	python3-numpy \
+	python3-setuptools \
+	ipython3 \
+	python3-pip \
+	python3-pandas \
+	zsh \
+	gnupg \
+	fontconfig \
+	openssl \
+	unrar \
+	encfs \
+	tig \
+	git \
+	xfonts-terminus \
+	scala \
+	kazam \
+	chromium-browser \
+	virtualbox \
+	virtualbox-guest-additions-iso \
+	vlc \
+	sublime-text-installer
+
+# If we are inside VirtualBox, add current user to VBox groups
+SYSTEMNAME=`sudo dmidecode -s system-product-name`
+if [ "$SYSTEMNAME" = "VirtualBox" ]; then
+	sudo adduser $USER vboxusers
+	sudo adduser $USER vboxsf
+fi
+
+# Fonts
+mkdir ~/.fonts
+cp -a ./data/fonts/* ~/.fonts/
+
+# Xfce configuration
+xfconf-query --channel xfce4-panel --property "/panels/panel-0/mode" --set 2
+xfconf-query --channel xfce4-panel --property "/panels/panel-0/size" --set 64
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Super>Down" --create --type string --set "tile_down_right_key"
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Super>Left" --create --type string --set "tile_left_key"
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Super>Right" --create --type string --set "tile_right_key"
+xfconf-query --channel xfce4-keyboard-shortcuts --property "/xfwm4/custom/<Super>Up" --create --type string --set "tile_up_right_key"
+cp -r ./data/terminal ~/.config/xfce4/
+
+# Emacs config
+git clone https://github.com/vurmux/.emacs.d.git
+rm ~/.emacs
+rm -rf ~/.emacs.d
+mv .emacs.d ~
+
+# Sublime text config
+mkdir -p ~/.config/sublime-text-3/Packages/User
+cp ./data/sublime-text/crazyfruits.tmTheme ~/.config/sublime-text-3/Packages
+cp ./data/sublime-text/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User
+
+echo "Finished!"
